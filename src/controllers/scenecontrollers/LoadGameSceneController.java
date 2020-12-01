e2120c4538ceb2bce13728e9a1a08e1100382cbc
Goktug Gurbuzturk <ggurbuzturk@gmail.com>
2020-12-01 16:18:18 +0300

    Back button is added and the size of the button is made bigger

--- src/controllers/scenecontrollers/LoadGameSceneController.java
+++ src/controllers/scenecontrollers/LoadGameSceneController.java
@@ -1,12 +1,18 @@
 package controllers.scenecontrollers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HorizontalDirection;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.VBox;
 import storage.filemanager.DataManager;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.net.URL;
@@ -27,16 +33,20 @@ public class LoadGameSceneController implements Initializable {
         buttonBox.setMaxHeight(700);
         buttonBox.setAlignment(Pos.CENTER);
         buttonBox.setStyle("-fx-padding: 50;");
-        buttonBox.setSpacing(10.0);
+        buttonBox.setSpacing(15.0);
         DataManager dataManager = DataManager.getInstance();
         List<String> savedNames = dataManager.getSavedNames();
-        ArrayList<Button> buttons = new ArrayList<Button>();
         for ( String saveName: savedNames ) {
             Button newButton = new Button(saveName);
             newButton.setMinWidth(buttonBox.getWidth());
-            newButton.setPrefWidth(200);
+            newButton.setPrefWidth(300);
+            newButton.setPrefHeight(100);
             buttonBox.getChildren().add(newButton);
         }
         loadBox.getChildren().add(buttonBox);
+    }
+
+    public void handleBackButton(ActionEvent actionEvent) throws IOException {
+        SceneManager.getInstance().showMainMenuScene();
     }
 }
