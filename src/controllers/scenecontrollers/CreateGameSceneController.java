d01cbe5783523e99ae770be7be91fc5762c71903
Bulut Gozubuyuk <bulutgozubuyuk@gmail.com>
2020-12-09 23:11:08 +0300

    Variable renaming and some comments

--- src/controllers/scenecontrollers/CreateGameSceneController.java
+++ src/controllers/scenecontrollers/CreateGameSceneController.java
@@ -31,8 +31,20 @@ import java.util.ResourceBundle;
  */
 public class CreateGameSceneController implements Initializable {
 
+    @FXML
+    private ListView<PlayerHBoxCell> listView;
+
     private static final ObservableList<PlayerHBoxCell> playerList = FXCollections.observableArrayList();
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        listView.setStyle("-fx-control-inner-background: #000a15; -fx-background-insets: 0 ;");
+        listView.setItems(playerList);
+    }
+
+    /**
+     * A method to handle start game button
+     */
     public void handleStartGameButton() {
         ArrayList<Player> players = new ArrayList<>();
         for (PlayerHBoxCell cell : playerList) {
@@ -43,10 +55,16 @@ public class CreateGameSceneController implements Initializable {
         SceneManager.getInstance().showGameScene();
     }
 
+    /**
+     * A method to handle exit button
+     */
     public void handleCreateGameExitButton() {
         SceneManager.getInstance().showMainMenuScene();
     }
 
+    /**
+     * A method to handle add player button
+     */
     public void handleAddPlayerButton() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../views/popupViews/addPlayerPopup.fxml"));
@@ -60,18 +78,18 @@ public class CreateGameSceneController implements Initializable {
             stage.initStyle(StageStyle.UNDECORATED);
             stage.setScene(scene);
             stage.showAndWait();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
     public static class PlayerHBoxCell extends HBox {
         ImageView avatar = new ImageView();
-        Label label = new Label();
+        Label nameLabel = new Label();
 
         //debug
-        Label label3 = new Label();
+        Label idLabel = new Label();
 
         playerComboBox colorBox;
         Button avatarButton = new Button();
@@ -91,6 +109,7 @@ public class CreateGameSceneController implements Initializable {
                 "grey",
                 "brown"};
 
+        // Constructor
         public PlayerHBoxCell(Player player) {
             super();
 
@@ -99,14 +118,14 @@ public class CreateGameSceneController implements Initializable {
             Color playerColor = getFXColor(player.getColor());
             this.setBackground(new Background(new BackgroundFill(playerColor, CornerRadii.EMPTY, Insets.EMPTY)));
 
-            label.setText(player.getName());
-            label.setMaxWidth(Double.MAX_VALUE);
-            HBox.setHgrow(label, Priority.ALWAYS);
+            nameLabel.setText(player.getName());
+            nameLabel.setMaxWidth(Double.MAX_VALUE);
+            HBox.setHgrow(nameLabel, Priority.ALWAYS);
 
             //debug
-            label3.setText(String.valueOf(player.getId()));
-            label3.setMaxWidth(Double.MAX_VALUE);
-            HBox.setHgrow(label3, Priority.ALWAYS);
+            idLabel.setText(String.valueOf(player.getId()));
+            idLabel.setMaxWidth(Double.MAX_VALUE);
+            HBox.setHgrow(idLabel, Priority.ALWAYS);
 
             colorBox = new playerComboBox(FXCollections.observableArrayList(colors), player);
             colorBox.getSelectionModel().select(player.getColor());
@@ -140,21 +159,12 @@ public class CreateGameSceneController implements Initializable {
                 }
             });
 
-            this.getChildren().addAll(avatar, label, label3, avatarButton, colorBox, deleteButton);
+            this.getChildren().addAll(avatar, nameLabel, idLabel, avatarButton, colorBox, deleteButton);
         }
-
         public Player getPlayer() {
             return player;
         }
-    }
 
-    @FXML
-    private ListView<PlayerHBoxCell> lView;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        lView.setStyle("-fx-control-inner-background: #000a15; -fx-background-insets: 0 ;");
-        lView.setItems(playerList);
     }
 
     public static Color getFXColor(String colorString){
