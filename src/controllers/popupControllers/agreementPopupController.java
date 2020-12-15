ee7a2097cb2a9c2512de4452b1eac25704fe4072
Bulut Gozubuyuk <bulutgozubuyuk@gmail.com>
2020-12-15 21:48:46 +0300

    Created template for popups

---
+++ src/controllers/popupControllers/agreementPopupController.java
@@ -0,0 +1,11 @@
+package controllers.popupControllers;
+
+import models.Agreement;
+
+public class agreementPopupController {
+    private Agreement agreement;
+
+    public void setAgreement(Agreement agreement){
+        this.agreement = agreement;
+    }
+}
---
+++ src/controllers/popupControllers/sellBuildingPopupController.java
@@ -0,0 +1,4 @@
+package controllers.popupControllers;
+
+public class sellBuildingPopupController {
+}
