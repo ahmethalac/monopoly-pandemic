d978846067fb92b2a59d9e53262e314dd21095b3
Bulut Gozubuyuk <bulutgozubuyuk@gmail.com>
2020-12-15 23:39:42 +0300

    FXML layout implementation for buy building

--- src/controllers/popupControllers/buyBuildingPopupController.java
+++ src/controllers/popupControllers/buyBuildingPopupController.java
@@ -3,10 +3,13 @@ package controllers.popupControllers;
 import controllers.modelcontrollers.GameManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import models.City;
 import models.Player;
 
@@ -18,6 +21,11 @@ public class buyBuildingPopupController {
     @FXML
     public ListView<BorderPane> cityView;
 
+    @FXML
+    void closeButtonClicked(ActionEvent event) {
+        closeStage(event);
+    }
+
     public buyBuildingPopupController(){
         player = GameManager.getInstance().getCurrentPlayer();
     }
@@ -26,17 +34,23 @@ public class buyBuildingPopupController {
 
         ArrayList<City> cities = player.getCities();
 
-        BorderPane[] rentBoxList = new BorderPane[cities.size()];
+        BorderPane[] cityList = new BorderPane[cities.size()];
 
         for (int i = 0; i < cities.size(); i++) {
-            rentBoxList[i] = new BorderPane();
-            rentBoxList[i].setLeft(new Label("" + cities.get(i)));
+            cityList[i] = new BorderPane();
+            cityList[i].setLeft(new Label("" + cities.get(i)));
         }
 
-        ObservableList<BorderPane> rentList = FXCollections.observableArrayList(rentBoxList);
+        ObservableList<BorderPane> rentList = FXCollections.observableArrayList(cityList);
         cityView.setItems(rentList);
     }
     public void buyOneHouse(){
 
+    }
+
+    private void closeStage(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
     }
 }
