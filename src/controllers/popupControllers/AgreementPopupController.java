3d613181d4d7ec73089d96ef3304427ef61d75d6
Bulut Gozubuyuk <bulutgozubuyuk@gmail.com>
2020-12-17 17:03:12 +0300

    Agreement popup working dropdown selection

--- src/controllers/popupControllers/AgreementPopupController.java
+++ src/controllers/popupControllers/AgreementPopupController.java
@@ -1,10 +1,23 @@
 package controllers.popupControllers;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import models.Agreement;
+import utils.ColorUtil;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AgreementPopupController extends PopupController {
+public class AgreementPopupController extends PopupController implements Initializable {
+    @FXML
+    public ComboBox<String> choosePlayerBox;
+    @FXML
+    public ComboBox<String> chooseOfferBox;
+    @FXML
+    public ComboBox<String> chooseOppositeOfferBox;
+
     private Agreement agreement;
 
     @FXML
@@ -19,5 +32,13 @@ public class AgreementPopupController extends PopupController {
 
     public void setAgreement(Agreement agreement){
         this.agreement = agreement;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("aggrement popup initialized");
+        choosePlayerBox.setItems(FXCollections.observableArrayList(ColorUtil.colors));
+        chooseOfferBox.setItems(FXCollections.observableArrayList(ColorUtil.colors));
+        chooseOppositeOfferBox.setItems(FXCollections.observableArrayList(ColorUtil.colors));
     }
 }
