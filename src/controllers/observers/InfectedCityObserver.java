e315badc6ec3d5de8590424e8f8f4cf045281cef
Ahmet Feyzi Halaç <ahmetfeyzihalac@gmail.com>
2020-12-20 02:58:11 +0300

    Infected City Observer fully implemented

--- src/controllers/observers/InfectedCityObserver.java
+++ src/controllers/observers/InfectedCityObserver.java
@@ -1,10 +1,17 @@
 package controllers.observers;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.scene.Group;
 import javafx.scene.shape.MeshView;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
 import models.City;
 import storage.filemanager.MeshImporter;
 
+import java.sql.Time;
 import java.util.ArrayList;
 
 public class InfectedCityObserver extends Observer {
@@ -23,11 +30,31 @@ public class InfectedCityObserver extends Observer {
     @Override
     public void update() {
         if ( sceneItems != null) {
-            if (((City) subject).isInfected()) {
-                MeshView virus = MeshImporter.getVirus();
-                virus.setTranslateX(coordinates[0]);
-                virus.setTranslateY(coordinates[1]);
-                sceneItems.getChildren().add(virus);
+            if (((City) subject).isInfected() && viruses.isEmpty()) {
+                int angle = 0;
+                while ( angle <= 360 ){
+                    MeshView virus = MeshImporter.getVirus();
+                    double yOffset = 35 * Math.cos(angle * Math.PI / 180);
+                    double xOffset = 35 * Math.sin(angle * Math.PI / 180);
+
+                    virus.setTranslateX(coordinates[0] + xOffset);
+                    virus.setTranslateY(coordinates[1] + yOffset);
+
+                    Rotate rotate = new Rotate();
+                    rotate.setPivotX(-xOffset);
+                    rotate.setPivotY(-yOffset);
+
+                    virus.getTransforms().add(rotate);
+                    Timeline timeline = new Timeline(
+                            new KeyFrame(Duration.ZERO, new KeyValue(rotate.angleProperty(), 0)),
+                            new KeyFrame(Duration.seconds(3), new KeyValue(rotate.angleProperty(), 360)));
+                    timeline.setCycleCount(Timeline.INDEFINITE);
+                    timeline.play();
+
+                    sceneItems.getChildren().add(virus);
+                    viruses.add(virus);
+                    angle += 90;
+                }
             } else {
                 sceneItems.getChildren().removeAll(viruses);
             }
