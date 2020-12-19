72227b195b02405ca8774300c86539b9d6342f5b
ege-sahin <sahin.ege98@gmail.com>
2020-12-19 15:52:13 +0300

    chance region popup started

---
+++ src/controllers/popupControllers/ChanceRegionPopupController.java
@@ -0,0 +1,16 @@
+package controllers.popupControllers;
+
+import controllers.modelcontrollers.GameManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import models.Card;
+
+public class ChanceRegionPopupController {
+    private Card chanceCard;
+
+    @FXML private Label label;
+
+    public void setLabel(){
+        // card ın getCardString metodu çağrılacak
+    }
+}
