0f45344d1594fdbb583953ca922afebd22a4d8d9
ege-sahin <sahin.ege98@gmail.com>
2020-12-19 23:33:25 +0300

    chance region popup

--- src/controllers/popupControllers/ChanceRegionPopupController.java
+++ src/controllers/popupControllers/ChanceRegionPopupController.java
@@ -3,18 +3,24 @@ package controllers.popupControllers;
 import controllers.modelcontrollers.GameManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
-public class ChanceRegionPopupController extends PopupController{
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML private Label label;
+public class ChanceRegionPopupController extends PopupController implements Initializable {
 
-    public void setLabel(){
-        label.setText(GameManager.getInstance().getCurrentChanceCard().getCardString());
-    }
+    @FXML private Text text;
 
     @FXML
     void closeButtonClicked(ActionEvent event) {
         closeStage(event);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        text.setText(GameManager.getInstance().getCurrentChanceCard().getCardString());
     }
 }
